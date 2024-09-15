770de494500e2ff40c58c179e1af60b8d6eaa83f
Inna Didenko <innadidenko29@gmail.com>
2024-09-15 21:00:06 +0300

    delete test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/react-arrow-game/",
+  base: "/arrow-game/",
   plugins: [react()],
   server: {
     open: true,
